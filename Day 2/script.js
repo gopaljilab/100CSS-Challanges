dc112c501a3da7660db1918e93689805b0a9fe74
gopaljilab <gopaljilab@gmail.com>
2024-08-11 22:41:01 +0530

    Updating The DAY to all Code Files

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,4 +1,13 @@
-$('.menu-icon').bind('click', function() {
-    $(this).toggleClass('active');
-    $(this).find('div').removeClass('no-animation');
-})
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", (event) => {
+	//attach a class called forward to parent
+	if(menu.classList.contains('forward')) {
+		menu.classList.replace("forward", "backward");
+	}
+	else if(menu.classList.contains('backward')) {
+		menu.classList.replace("backward", "forward");
+	}
+	else {
+		menu.classList.add('forward')
+	}
+});
